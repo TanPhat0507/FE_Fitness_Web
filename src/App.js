ef511045e8f987d2e1305eb012f6bd6f49fb1578
DilysT <21522624@gm.uit.edu.vn>
2024-11-05 14:25:54 +0700

    update code Authenticaiton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
-import './App.css';
+import Signup from './components/pages/Signup';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 const App = () => {
+  const [isSignupComplete, setIsSignupComplete] = useState(false);
+
+  const completeSignup = () => {
+    setIsSignupComplete(true);
+  };
 
   return (
-    <p>Hello</p>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Navigate to="/signup" replace />} 
+          />
+          <Route 
+            path="/signup" 
+            element={<Signup onComplete={completeSignup} />} 
+          />
+         
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
