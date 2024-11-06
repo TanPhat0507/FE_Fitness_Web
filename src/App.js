49e0335b4028e785ff74f3d6e518148bdfe0092a
FATDZ\ACER <21522444@gm.uit.edu.vn>
2024-11-06 22:39:13 +0700

    comple link to pages and build the structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React, { useState } from 'react';
-import './App.css';
+// import Signup from './components/pages/Signup.js';
+// import Login from './components/pages/Login.js'; // Import the Login component
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import Resetpw from './components/pages/Resetpw.js';
 
 const App = () => {
+  const [isSignupComplete, setIsSignupComplete] = useState(false);
+
+  const completeSignup = () => {
+    setIsSignupComplete(true);
+  };
 
   return (
-    <p>Hello</p>
+    <Router>
+      <div className="app">
+        <Routes>
+          {/* <Route
+            path="/"
+            element={<Navigate to="/signup" replace />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup onComplete={completeSignup} />}
+          />
+          <Route
+            path="/login"
+            element={<Login />} // Add the route for Login
+          />
+          <Route
+            path="/resetpw"
+            element={<Resetpw />}
+          /> */}
+
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
